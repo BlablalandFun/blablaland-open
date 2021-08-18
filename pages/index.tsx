4eb9eeafe7ed755f06d7c7240f6ee329fdff0a4f
Quentin <31007601+Yovach@users.noreply.github.com>
2021-08-18 14:36:00 +0200

    feat: init project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-white text-blue-500">
+      Salut à tous les amis !
+    </div>
+  )
+}
+
+export default Home
