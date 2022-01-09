3a02a41e6c191eb1bf236fce88eef2c2d0d242a4
Quentin <31007601+Yovach@users.noreply.github.com>
2022-01-09 23:08:05 +0100

    fix: export const prisma

--- packages/database/index.js
+++ packages/database/index.js
@@ -1,8 +1,6 @@
 import client from '@prisma/client'
 
-const prisma = global.prisma || new client.PrismaClient();
+export const prisma = global.prisma || new client.PrismaClient();
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
-
-export const prisma;
