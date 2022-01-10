78c149011b546f6d5fe1f3604e1958cd333e407d
Quentin <31007601+Yovach@users.noreply.github.com>
2022-01-10 23:01:45 +0100

    feat: début inscription

---
+++ packages/client/pages/api/auth/register.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "@blablaland/db";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const errors = {};
+  const { password, username, confirmPassword } = req.body;
+
+  if (!password || !username || !confirmPassword) {
+    if (!username) {
+      errors["username"] = "Un pseudo doit être renseigné";
+    }
+    if (!password) {
+      errors["password"] = "Un mot de passe doit être renseigné";
+    }
+    if (!confirmPassword) {
+      errors["confirmPassword"] = "Tu dois confirmer ton mot de passe";
+    }
+  } else if (username.length < 3 || username.length > 12) {
+    errors["username"] = "Les mots de passe ne correspondent pas";
+  } else if (password !== confirmPassword) {
+    errors["confirmPassword"] = "Le pseudo doit faire entre 3 et 12 caractères";
+  }
+
+  // il y a des erreurs
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json({ errors });
+  }
+
+  
+}
---
+++ packages/client/pages/register.tsx
@@ -0,0 +1,67 @@
+import type { NextPage } from "next";
+
+import Image from "next/image";
+import { FormEvent } from "react";
+import logo from "../assets/logo_blablaland.png";
+import { Layout } from "../components/Layout";
+
+const RegisterPage: NextPage = () => {
+  async function onSubmit(evt: FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+
+    const formData = new FormData(evt.currentTarget);
+    const req = await fetch("/api/auth/register", {
+      body: JSON.stringify(Object.fromEntries(formData)),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    // const res = await req.json();
+    console.log(evt);
+  }
+
+  return (
+    <Layout>
+      <div className="p-5">
+        <h1 className="font-medium text-2xl text-slate-100">Inscris-toi !</h1>
+      </div>
+      <form className="flex flex-col p-5 gap-y-10 flex-1" onSubmit={onSubmit}>
+        <div className="flex flex-col gap-y-1">
+          <label htmlFor="username" className="text-slate-100 font-medium">
+            Entre ton nom d'utilisateur :
+          </label>
+          <input
+            id="username"
+            name="username"
+            className="px-1 py-1 text-white bg-transparent border-b-2 border-slate-500 focus:border-sky-500 focus:outline-none"
+          />
+        </div>
+        <div className="flex flex-col gap-y-1">
+          <label htmlFor="password" className="text-slate-100 font-medium">
+            Entre ton mot de passe :
+          </label>
+          <input
+            id="password"
+            name="password"
+            className="px-1 py-1 text-white bg-transparent border-b-2 border-slate-500 focus:border-sky-500 focus:outline-none"
+          />
+        </div>
+        <div className="flex flex-col gap-y-1">
+          <label htmlFor="confirmPassword" className="text-slate-100 font-medium">
+            Confirme ton mot de passe :
+          </label>
+          <input
+            id="confirmPassword"
+            name="confirmPassword"
+            className="px-1 py-1 text-white bg-transparent border-b-2 border-slate-500 focus:border-sky-500 focus:outline-none"
+          />
+        </div>
+        <button className="self-center px-6 py-2 rounded-full font-medium text-white bg-sky-600 hover:bg-sky-700">Confirme ton inscription</button>
+      </form>
+    </Layout>
+  );
+};
+
+export default RegisterPage;
