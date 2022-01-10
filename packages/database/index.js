1851c47d1b76392629b97b24690f8adc4dae2269
Quentin <31007601+Yovach@users.noreply.github.com>
2022-01-10 21:52:37 +0100

    fix: import prismaclient

--- packages/database/index.js
+++ packages/database/index.js
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import prismaLib from '@prisma/client'
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma = global.prisma || new prismaLib.PrismaClient();
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
