3d4ac6fa7fb132a553ad671b28b050c3db032dc2
Quentin <31007601+Yovach@users.noreply.github.com>
2022-01-12 23:46:45 +0100

    feat: ajout infos page d'accueil

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -11,11 +11,22 @@ const Home: NextPage = () => {
 
   return (
     <Layout>
-      <div className="p-5">
+      <div className="p-5 flex-1">
         <h1 className="font-medium text-2xl text-slate-100">Serveur de développement</h1>
-        <p className="pt-2 text-slate-300 text-sm">
-          Il est <u>déconseillé</u> de l'utiliser en production !
-        </p>
+
+        <div className="text-slate-300 text-sm mt-2">
+          <p>Le projet est actuellement en développement et peut contenir de nombreux bogues.</p>
+
+          <p className="mt-2 ">
+            Pour plus d'informations, merci de vous rendre{" "}
+            <a className="text-blue-300 hover:text-blue-400" href="https://github.com/BlablalandFun/open">
+              ici
+            </a>
+            .
+          </p>
+
+          <p className="mt-10">Pour naviguer sur le site, vous pouvez utiliser les boutons à gauche.</p>
+        </div>
       </div>
     </Layout>
   );
