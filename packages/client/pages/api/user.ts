801a5c408a69e3a53a5d1c1a42c5b297d37559be
Quentin <31007601+Yovach@users.noreply.github.com>
2022-01-12 22:22:44 +0100

    feat(auth): destruction cookie si inactif

--- packages/client/pages/api/user.ts
+++ packages/client/pages/api/user.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { prisma } from "@blablaland/db";
 import type { NextApiRequest, NextApiResponse } from "next";
-import nookies from "nookies";
+import nookies, { destroyCookie } from "nookies";
 import { checkJwtAuth } from "../../src/helpers";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -30,6 +30,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
     return res.status(200).json({ username });
   } catch (err) {
+    destroyCookie({ res }, "SESSION", { path: "/" });
     return res.status(401).json({});
   }
 }
