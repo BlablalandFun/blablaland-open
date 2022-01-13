c6b8e9f0796698450b1fa154d4bbbc2fe27c20d4
Quentin <31007601+Yovach@users.noreply.github.com>
2022-01-13 14:56:48 +0100

    feat: début mode light

--- packages/client/components/MenuButton.tsx
+++ packages/client/components/MenuButton.tsx
@@ -6,14 +6,14 @@ type OnClickFunc = (e: EventClickParam) => void;
 export function MenuButton(props: PropsWithChildren<{ href: string | OnClickFunc; title: string }>) {
   if (typeof props.href === "string") {
     return (
-      <a title={props.title} href={props.href} className="bg-white/20 p-3 rounded-full hover:bg-blue-300/20 text-blue-200">
+      <a title={props.title} href={props.href} className="bg-blue-300 dark:bg-white/20 p-3 rounded-full hover:bg-blue-400 dark:hover:bg-blue-300/20 text-blue-100 dark:text-blue-200">
         {props.children}
       </a>
     );
   }
 
   return (
-    <button title={props.title} onClick={props.href} className="bg-white/20 p-3 rounded-full hover:bg-blue-300/20 text-blue-200">
+    <button title={props.title} onClick={props.href} className="bg-blue-300 dark:bg-white/20 p-3 rounded-full hover:bg-blue-400 dark:hover:bg-blue-300/20 text-blue-100 dark:text-blue-200">
       {props.children}
     </button>
   );
