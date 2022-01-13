db5e39273c1d0172992f028a14514ee581db039d
Quentin <31007601+Yovach@users.noreply.github.com>
2022-01-13 15:54:56 +0100

    feat: ajout session dans les paramètres du chat.swf

--- packages/client/pages/play.tsx
+++ packages/client/pages/play.tsx
@@ -1,31 +1,51 @@
-import type { NextPage } from "next";
-import { useAuthContext } from "../components/AuthStore";
+import type { GetServerSideProps, NextPage } from "next";
+import nookies, { destroyCookie } from "nookies";
 import { Layout } from "../components/Layout";
 import { SwfContainer } from "../components/SwfContainer";
+import { checkJwtAuth } from "../src/helpers";
 
-const Home: NextPage = () => {
+type PlayProps = {
+  SESSION: string;
+};
+
+const PlayPage: NextPage<PlayProps> = (props: PlayProps) => {
   const FLASH_VARS = {
-    SESSION: "TEST_SESSION",
+    SESSION: props.SESSION,
     DAILYMSG: "Projet open-source",
     DAILYMSGSECU: "Message de sécurité",
     CACHE_VERSION: 1,
   };
 
-  const auth = useAuthContext();
-
   return (
     <Layout className="flex flex-col bg-white/10 backdrop-blur rounded-xl my-4">
       <div className="p-5 inline-flex items-center">
         <h1 className="flex-1 font-medium text-2xl text-slate-100">Serveur de développement</h1>
-        {auth?.data?.username && (
-          <a title="Gérer mon compte" href="/my-account" className="font-medium text-2xl text-blue-300 hover:text-blue-400">
-            {auth.data.username}
-          </a>
-        )}
       </div>
       <SwfContainer src="/chat/chat.swf" className="self-center max-w-[950px] max-h-[560px]" flashVars={FLASH_VARS} height={560} width={950} />
     </Layout>
   );
 };
 
-export default Home;
+export default PlayPage;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { SESSION } = nookies.get(ctx);
+  if (!SESSION) {
+    try {
+      await checkJwtAuth(SESSION);
+    } catch (err) {
+      destroyCookie(ctx, "SESSION");
+      return {
+        props: {},
+        redirect: {
+          destination: "/login",
+        },
+      };
+    }
+  }
+  return {
+    props: {
+      SESSION,
+    },
+  };
+};
